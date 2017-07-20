b7f5ab44ddefe39e99dc76d961b6ed17dffdf475
Vega Silva <Diego.Vega@tecnocom.es>
2017-07-20 12:52:33 +0200

    state fixed

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -19,7 +19,7 @@ class SearchBar extends Component {
     }
 
     onInputChange(term) {
-        //this.setState({ term: term });
+        this.setState({ term: term });
         this.props.onSearchTermChange(term);
     }
 }
